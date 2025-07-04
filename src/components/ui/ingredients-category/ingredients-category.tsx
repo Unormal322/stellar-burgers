a9574cc6fd1ee49f17b64c62255e51fd9566508d
Gregory Reshetnik <unormal322@yandex.ru>
2025-07-04 15:37:54 +0800

    localhost вынесен в baseUrl, создана константа для оптимизации тестов

--- src/components/ui/ingredients-category/ingredients-category.tsx
+++ src/components/ui/ingredients-category/ingredients-category.tsx
@@ -6,36 +6,31 @@ import { BurgerIngredient } from '@components';
 export const IngredientsCategoryUI = forwardRef<
   HTMLUListElement,
   TIngredientsCategoryUIProps
->(({ title, titleRef, ingredients, ingredientsCounters }, ref) => {
-  // ВРЕМЕННО для отладки:
-  console.log('Category:', title, 'Ingredients:', ingredients);
-
-  return (
-    <>
-      <h3 className='text text_type_main-medium mt-10 mb-6' ref={titleRef}>
-        {title}
-      </h3>
-      <ul
-        className={styles.items}
-        ref={ref}
-        data-cy={
-          title === 'Булки'
-            ? 'bun-ingredients'
-            : title === 'Начинки'
-              ? 'mains-ingredients'
-              : title === 'Соусы'
-                ? 'sauces-ingredients'
-                : undefined
-        }
-      >
-        {ingredients.map((ingredient) => (
-          <BurgerIngredient
-            ingredient={ingredient}
-            key={ingredient._id}
-            count={ingredientsCounters[ingredient._id]}
-          />
-        ))}
-      </ul>
-    </>
-  );
-});
+>(({ title, titleRef, ingredients, ingredientsCounters }, ref) => (
+  <>
+    <h3 className='text text_type_main-medium mt-10 mb-6' ref={titleRef}>
+      {title}
+    </h3>
+    <ul
+      className={styles.items}
+      ref={ref}
+      data-cy={
+        title === 'Булки'
+          ? 'bun-ingredients'
+          : title === 'Начинки'
+            ? 'mains-ingredients'
+            : title === 'Соусы'
+              ? 'sauces-ingredients'
+              : undefined
+      }
+    >
+      {ingredients.map((ingredient) => (
+        <BurgerIngredient
+          ingredient={ingredient}
+          key={ingredient._id}
+          count={ingredientsCounters[ingredient._id]}
+        />
+      ))}
+    </ul>
+  </>
+));
